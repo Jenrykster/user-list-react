3604e2e4401bc63df534e0458546c259fbb2bec7
João Henrique <joaoenrique.san@gmail.com>
2022-01-10 16:24:21 -0300

    feat: add modal and validation

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import Modal from './components/UI/Modal';
 import UserForm from './components/UserForm/UserForm';
 import UserList from './components/UserList/UserList';
 
@@ -10,6 +11,8 @@ function App() {
       id: '01',
     },
   ]);
+  let [showModal, setShowModal] = useState(false);
+  let [modalError, setModalError] = useState('There was an error');
   const deleteHandler = (id) => {
     setUsers((prevState) => {
       return prevState.filter((user) => {
@@ -18,15 +21,31 @@ function App() {
     });
   };
   const createNewUser = (userData) => {
-    setUsers((prevState) => {
-      return [...prevState, userData];
-    });
+    if (userData.userName.trim().length === 0 || userData.age.length === 0) {
+      setModalError('Please insert valid data');
+      setShowModal(true);
+    } else if (parseInt(userData.age) < 0) {
+      setModalError("Age can't be a negative number");
+      setShowModal(true);
+    } else {
+      setUsers((prevState) => {
+        return [...prevState, userData];
+      });
+    }
+  };
+  const dismissModal = (event) => {
+    setShowModal(false);
   };
 
   return (
     <div>
       <UserForm onAddUser={createNewUser} />
       <UserList users={users} onDelete={deleteHandler} />
+      {showModal && (
+        <Modal title={'Invalid input'} onModalDismissal={dismissModal}>
+          {modalError}
+        </Modal>
+      )}
     </div>
   );
 }
